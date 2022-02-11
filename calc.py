68341de853417d0e3ffd65d1237701f3a0631fe1
Kyle Pace <kyle.pace@documo.com>
2022-02-10 18:55:42 -0600

    working on multiline expressions

--- calc.py
+++ calc.py
@@ -1,5 +1,5 @@
 from getInput import getInput
 
-print('Welcome to the reverse polish calculator')
+print('\nWelcome to the reverse polish calculator')
 
 getInput()
--- getInput.py
+++ getInput.py
@@ -1,9 +1,12 @@
-from ast import operator
 from arithmetic import add, subtract, multiply, divide
 
 calcArray = []
 
 def calculateResult():
+
+	def reInsert():
+		inputList.insert(currIdx - 2, str(value))
+
 	inputList = calcArray[0].split(' ')
 	while len(inputList) >= 3:
 
@@ -15,67 +18,67 @@ def calculateResult():
 				currOperator = inputList.pop(currIdx - 2)
 				if currOperator == '+':
 					value = add(firstOperand, secondOperand)
-					inputList.insert(currIdx - 2, str(value))
-				if currOperator == '*':
+					reInsert()
+				elif currOperator == '*':
 					value = multiply(firstOperand, secondOperand)
-					inputList.insert(currIdx - 2, str(value))
-				if currOperator == '-':
+					reInsert()
+				elif currOperator == '-':
 					value = subtract(firstOperand, secondOperand)
-					inputList.insert(currIdx - 2, str(value))
-				if currOperator == '/':
+					reInsert()
+				else:
 					value = divide(firstOperand, secondOperand)
-					inputList.insert(currIdx - 2, str(value))
+					reInsert()
 
 			currIdx += 1
 
 	print('\nsolution', float(inputList[0]), '\n' )
-
+	
 def getInput():
 	while True:
 		try:
-			calculation = input('Please ener your calculation in reverse polish notation: ')
+			if len(calcArray) != 0:
+				clearSolution = input('would you like to clear previous solution? "y" or "n": ')
+				if clearSolution == 'y':
+					calcArray.clear()
+				else:
+					newInput = input('\nPlease enter the rest of your expression: ')
+					print('new input', newInput)
+					newCalcArray = calcArray[0] + " " + newInput
+					print('new calc array', newCalcArray)
+					calcArray.clear()
+					calcArray.append(newCalcArray)
+					print('calcarray', calcArray)
+					checkInfo = input('\nDone entering info? Type "y" to calculate otherwise type "n" to continue entering: ')
+					if checkInfo == "y":
+						calculateResult()
+
+
+
+			calculation = input('\nPlease ener your calculation in reverse polish notation: ')
 		except ValueError:
-			print("Sorry, let's try that input again")
+			# print("Sorry, let's try that input again")
 			continue
 
 		if calculation.isalpha():
-			print("Please provide a valid expression to calculate (Maybe try entering numbers!)")
+			print("\nPlease provide a valid expression to calculate (Maybe try entering numbers!)")
 			continue
 
-		if calculation[-1] not in ('+-/*'):
-			print("Please make sure the calculation is in reverse polish notation (needs to end in + - / or *)")
-			continue
 		else:
 			calcArray.append(calculation)
 
-			checkInfo = input('Done entering info? Type "y" to calculate otherwise type "n" to continue entering: ')
+			checkInfo = input('\nDone entering info? Type "y" to calculate otherwise type "n" to continue entering: ')
 
 			if checkInfo == "y":
 				calculateResult()
 			else:
-				continue
-
-
-			
-
-
-
-
-
-
-
-
-
-
-
-
-	# if calculation[-1] == '+':
-	# 	print(add(calculation[0], calculation[2]))
-	# elif calculation[-1] == '-':
-	# 	print(subtract(calculation[0], calculation[2]))
-	# elif calculation[-1] == '*':
-	# 	print(multiply(calculation[0], calculation[2]))
-	# elif calculation[-1] == '/':
-	# 	print(divide(calculation[0], calculation[2]))
-	# else:
-	# 	print('Sorry, try again')
+				while True:
+					newInput = input('\nPlease enter the rest of your expression: ')
+					newCalcArray = calcArray[0] + " " + newInput
+					calcArray.clear()
+					calcArray.append(newCalcArray)
+					checkInfo = input('\nDone entering info? Type "y" to calculate otherwise type "n" to continue entering: ')
+					if checkInfo == "y":
+						calculateResult()
+						break
+					else:
+						continue
